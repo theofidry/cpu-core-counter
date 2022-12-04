c68c749e5ebfbafec3829064bd466dbb7089fc44
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2022-12-04 17:50:34 +0100

    Consolidate WindowsWmicFinder (#18)
    Split the "get the string which contains the info" and the "retrieve the info from the string" in order to add unit tests to the 2nd one.

---
+++ tests/WindowsWmicTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Fidry CPUCounter Config package.
+ *
+ * (c) Théo FIDRY <theo.fidry@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Fidry\CpuCounter\Test;
+
+use Fidry\CpuCounter\WindowsWmicFinder;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Fidry\CpuCounter\WindowsWmicFinder
+ *
+ * @internal
+ */
+final class WindowsWmicTest extends TestCase
+{
+    /**
+     * @dataProvider wmicProvider
+     */
+    public function test_it_can_count_the_number_of_cpu_cores(
+        string $nproc,
+        ?int $expected
+    ): void {
+        $actual = WindowsWmicFinder::countCpuCores($nproc);
+
+        self::assertSame($expected, $actual);
+    }
+
+    public static function wmicProvider(): iterable
+    {
+        yield 'example from a Windows machine' => [
+            <<<'EOF'
+                3
+
+                EOF,
+            3,
+        ];
+    }
+}
