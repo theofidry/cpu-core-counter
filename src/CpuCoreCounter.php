61ce9eea9774e27f6eca3839e882d42f5caad1db
Théo FIDRY <theo.fidry@gmail.com>
2022-12-04 16:10:21 +0100

    Fix license

--- src/CpuCoreCounter.php
+++ src/CpuCoreCounter.php
@@ -11,15 +11,6 @@
 
 declare(strict_types=1);
 
-/**
- * Copyright (c) 2017-2022 Andreas Möller.
- *
- * For the full copyright and license information, please view
- * the LICENSE.md file that was distributed with this source code.
- *
- * @see https://github.com/ergebnis/php-package-template
- */
-
 namespace Fidry\CpuCounter;
 
 use function count;
