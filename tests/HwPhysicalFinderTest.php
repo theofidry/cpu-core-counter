a8ff2d975d76ae1e275e153d1115d612cd1629eb
Théo FIDRY <theo.fidry@gmail.com>
2022-12-07 02:08:59 +0100

    Add test

---
+++ tests/HwPhysicalFinderTest.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * This file is part of the Fidry CPUCounter Config package.
+ *
+ * (c) Théo FIDRY <theo.fidry@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Fidry\CpuCoreCounter\Test;
+
+use Fidry\CpuCoreCounter\HwPhysicalFinder;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Fidry\CpuCoreCounter\HwPhysicalFinder
+ *
+ * @internal
+ */
+final class HwPhysicalFinderTest extends TestCase
+{
+    /**
+     * @dataProvider processProvider
+     */
+    public function test_it_can_count_the_number_of_cpu_cores(
+        string $process,
+        ?int $expected
+    ): void {
+        $actual = HwPhysicalFinder::countCpuCores($process);
+
+        self::assertSame($expected, $actual);
+    }
+
+    public static function processProvider(): iterable
+    {
+        yield 'empty' => [
+            <<<'EOF'
+
+EOF
+            ,
+            null,
+        ];
+
+        yield 'whitespace' => [
+            <<<'EOF'
+ 
+EOF
+            ,
+            null,
+        ];
+
+        // MyMachine™
+        yield 'example from an OSX machine' => [
+            <<<'EOF'
+3
+
+EOF
+            ,
+            3,
+        ];
+        yield 'example with extra spaces' => [
+            <<<'EOF'
+ 3 
+
+EOF
+            ,
+            3,
+        ];
+
+        yield 'no processor' => [
+            <<<'EOF'
+0
+
+EOF
+            ,
+            null,
+        ];
+    }
+}
