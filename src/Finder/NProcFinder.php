7d2f4d275f060c92138e0898ac319364501eb418
Théo FIDRY <theo.fidry@gmail.com>
2022-12-10 19:10:50 +0100

    Refactor nproc into a procopen based finder

--- src/Finder/NProcFinder.php
+++ src/Finder/NProcFinder.php
@@ -29,10 +29,8 @@ use const PHP_EOL;
  * @see https://github.com/infection/infection/blob/fbd8c44/src/Resource/Processor/CpuCoresCountProvider.php#L69-L82
  * @see https://unix.stackexchange.com/questions/146051/number-of-processors-in-proc-cpuinfo
  */
-final class NProcFinder implements CpuCoreFinder
+final class NProcFinder extends ProcOpenBasedFinder
 {
-    private const DETECT_NPROC_COMMAND = 'command -v nproc';
-
     /**
      * @var bool
      */
@@ -46,72 +44,6 @@ final class NProcFinder implements CpuCoreFinder
         $this->all = $all;
     }
 
-    public function diagnose(): string
-    {
-        if (!function_exists('shell_exec')) {
-            return 'The function "shell_exec" is not available.';
-        }
-
-        try {
-            $commandNproc = ShellExec::execute(self::DETECT_NPROC_COMMAND);
-        } catch (ExecException $nprocCommandNotFound) {
-            return sprintf(
-                'The command nproc was not detected. The command "%s" failed: %s',
-                self::DETECT_NPROC_COMMAND,
-                $nprocCommandNotFound->getMessage()
-            );
-        }
-
-        if ('' === trim($commandNproc)) {
-            return sprintf(
-                'The command nproc was not detected. The command "%s" gave an empty output.',
-                self::DETECT_NPROC_COMMAND
-            );
-        }
-
-        // Redirect the STDERR to the STDOUT since popen cannot capture the
-        // STDERR.
-        // We could use proc_open but this would be a greater difference between
-        // the command we really execute when using the finder and what we will
-        // diagnose.
-        $nprocCommand = 'nproc'.($this->all ? ' --all' : '').' 2>&1';
-
-        try {
-            $nproc = ShellExec::execute($nprocCommand);
-        } catch (ExecException $nprocFailed) {
-            return sprintf(
-                'The command "%s" failed: %s',
-                $nprocCommand,
-                $nprocFailed->getMessage()
-            );
-        }
-
-        return sprintf(
-            'The command "%s" gave the following output:%s%s',
-            $nprocCommand,
-            PHP_EOL,
-            $nproc
-        );
-    }
-
-    /**
-     * @return positive-int|null
-     */
-    public function find(): ?int
-    {
-        if (!self::supportsNproc()) {
-            return null;
-        }
-
-        try {
-            $nproc = ShellExec::execute('nproc'.($this->all ? ' --all' : ''));
-        } catch (ExecException $nprocFailed) {
-            return null;
-        }
-
-        return self::countCpuCores($nproc);
-    }
-
     public function toString(): string
     {
         return sprintf(
@@ -120,19 +52,9 @@ final class NProcFinder implements CpuCoreFinder
         );
     }
 
-    private static function supportsNproc(): bool
+    protected function getCommand(): string
     {
-        if (!function_exists('shell_exec')) {
-            return false;
-        }
-
-        try {
-            $commandNproc = ShellExec::execute(self::DETECT_NPROC_COMMAND);
-        } catch (ExecException $nprocCommandNotFound) {
-            return false;
-        }
-
-        return '' !== trim($commandNproc);
+        return 'nproc'.($this->all ? ' --all' : '').' 2>&1';
     }
 
     /**
