072774313e5b6efaec0b3f1b1b4220903549888e
Théo FIDRY <theo.fidry@gmail.com>
2022-12-16 23:30:44 +0100

    Remove unnecessary code for NProcFinder

--- tests/Finder/NProcFinderTest.php
+++ tests/Finder/NProcFinderTest.php
@@ -55,7 +55,7 @@ final class NProcFinderTest extends TestCase
     }
 
     /**
-     * @dataProvider nprocProvider
+     * @dataProvider \Fidry\CpuCoreCounter\Test\Finder\ProcOpenBasedFinderTestCase::processResultProvider
      */
     public function test_it_can_count_the_number_of_cpu_cores(
         string $nproc,
@@ -64,51 +64,5 @@ final class NProcFinderTest extends TestCase
         $actual = NProcFinder::countCpuCores($nproc);
 
         self::assertSame($expected, $actual);
-    }
-
-    public static function nprocProvider(): iterable
-    {
-        yield 'empty' => [
-            <<<'EOF'
-
-EOF
-            ,
-            null,
-        ];
-
-        yield 'whitespace' => [
-            <<<'EOF'
- 
-EOF
-            ,
-            null,
-        ];
-
-        // $ docker run  --tty --rm --platform linux/amd64 alpine:3.14 nproc --all
-        yield 'example from an alpine Docker image' => [
-            <<<'EOF'
-3
-
-EOF
-            ,
-            3,
-        ];
-        yield 'example with extra spaces' => [
-            <<<'EOF'
- 3 
-
-EOF
-            ,
-            3,
-        ];
-
-        yield 'no processor' => [
-            <<<'EOF'
-0
-
-EOF
-            ,
-            null,
-        ];
     }
 }
