5127ac5842b2bbf25752c6fc25ff3c5e6c77ebde
Théo FIDRY <theo.fidry@gmail.com>
2024-02-07 07:29:33 +0100

    feat: Introduce Windows registry based finder

---
+++ tests/Finder/WindowsRegistryLogicalFinderTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the Fidry CPUCounter Config package.
+ *
+ * (c) Théo FIDRY <theo.fidry@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Fidry\CpuCoreCounter\Test\Finder;
+
+use Fidry\CpuCoreCounter\Executor\ProcessExecutor;
+use Fidry\CpuCoreCounter\Finder\ProcOpenBasedFinder;
+use Fidry\CpuCoreCounter\Finder\WindowsRegistryLogicalFinder;
+use Fidry\CpuCoreCounter\Finder\WmicLogicalFinder;
+
+/**
+ * @covers \Fidry\CpuCoreCounter\Finder\WindowsRegistryLogicalFinder
+ *
+ * @internal
+ */
+final class WindowsRegistryLogicalFinderTest extends ProcOpenBasedFinderTestCase
+{
+    protected function createFinder(ProcessExecutor $executor): ProcOpenBasedFinder
+    {
+        return new WindowsRegistryLogicalFinder($executor);
+    }
+
+    public static function processResultProvider(): iterable
+    {
+        yield 'empty output' => [
+            [
+                '',
+                '',
+            ],
+            null,
+        ];
+
+        yield 'example from the GitHub Actions machine' => [
+            [
+                <<<'EOF'
+[HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\0]
+[HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\2]
+EOF
+                ,
+                '',
+            ],
+            2,
+        ];
+
+        yield 'another example from the GitHub Actions machine' => [
+            [
+                <<<'EOF'
+[HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\0]
+[HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\2]
+
+EOF
+                ,
+                '',
+            ],
+            2,
+        ];
+    }
+}
