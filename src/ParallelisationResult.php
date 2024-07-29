c52b2dcbac0de68c91efb48fb247f991c19b9da4
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2024-07-29 11:32:13 +0200

    feat: Add an easier way to debug (#131)
    I would like to make cases like https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/issues/8129 easier to debug. There is 3 things to check:
    
    - If each respective finder behaves as expected, this is the job of the `diagnose.php`.
    - Which finder result got picked: the introduce `CpuCoreFinder::trace()` should answer that.
    - A way to debug `CpuCoreFinder::getAvailableForParallelisation()`. Since #127 is not released yet, the signature of `::getAvailableForParallelisation()` to return a result value object which could capture the resolved parameters to be able to figure out with which values the calculation was done.

---
+++ src/ParallelisationResult.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * This file is part of the Fidry CPUCounter Config package.
+ *
+ * (c) Théo FIDRY <theo.fidry@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Fidry\CpuCoreCounter;
+
+/**
+ * @readonly
+ */
+final class ParallelisationResult
+{
+    /**
+     * @var positive-int
+     */
+    public $passedReservedCpus;
+
+    /**
+     * @var positive-int|null
+     */
+    public $passedLimit;
+
+    /**
+     * @var float|null
+     */
+    public $passedLoadLimitPerCore;
+
+    /**
+     * @var float|null
+     */
+    public $passedSystemLoadAverage;
+
+    /**
+     * @var positive-int|null
+     */
+    public $correctedLimit;
+
+    /**
+     * @var float
+     */
+    public $correctedSystemLoadAverage;
+
+    /**
+     * @var positive-int
+     */
+    public $totalCoresCount;
+
+    /**
+     * @var positive-int
+     */
+    public $availableCpus;
+
+    /**
+     * @param positive-int      $passedReservedCpus
+     * @param positive-int|null $passedLimit
+     * @param positive-int      $totalCoresCount
+     * @param positive-int      $availableCpus
+     */
+    public function __construct(
+        int $passedReservedCpus,
+        ?int $passedLimit,
+        ?float $passedLoadLimitPerCore,
+        ?float $passedSystemLoadAverage,
+        ?int $correctedLimit,
+        float $correctedSystemLoadAverage,
+        int $totalCoresCount,
+        int $availableCpus
+    ) {
+        $this->passedReservedCpus = $passedReservedCpus;
+        $this->passedLimit = $passedLimit;
+        $this->passedLoadLimitPerCore = $passedLoadLimitPerCore;
+        $this->passedSystemLoadAverage = $passedSystemLoadAverage;
+        $this->correctedLimit = $correctedLimit;
+        $this->correctedSystemLoadAverage = $correctedSystemLoadAverage;
+        $this->totalCoresCount = $totalCoresCount;
+        $this->availableCpus = $availableCpus;
+    }
+}
