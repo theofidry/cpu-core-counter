3c14f39c1a99bf94a00f4aa3e8efd38f2af1a368
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2024-07-29 09:55:03 +0200

    feat: Adjust the CPUs core count available based on the recent system load (#130)

--- src/CpuCoreCounter.php
+++ src/CpuCoreCounter.php
@@ -16,6 +16,7 @@ namespace Fidry\CpuCoreCounter;
 use Fidry\CpuCoreCounter\Finder\CpuCoreFinder;
 use Fidry\CpuCoreCounter\Finder\EnvVariableFinder;
 use Fidry\CpuCoreCounter\Finder\FinderRegistry;
+use function sys_getloadavg;
 
 final class CpuCoreCounter
 {
@@ -38,28 +39,45 @@ final class CpuCoreCounter
     }
 
     /**
-     * @param positive-int      $reservedCpus
-     * @param positive-int|null $limit        If no limit is given, all available CPUs will be returned.
+     * @param positive-int $reservedCpus
      *
      * @return positive-int
+     *
+     * @see https://php.net/manual/en/function.sys-getloadavg.php
      */
     public function getAvailableForParallelisation(
         int $reservedCpus = 1,
-        ?int $limit = null
+        ?int $limit = null,
+        ?float $loadLimitPerCore = .9,
+        ?float $systemLoadAverage = null
     ): int {
-        $limit = null === $limit
+        $correctedLimit = null === $limit
             ? self::getKubernetesLimit()
             : $limit;
 
-        $count = $this->getCountWithFallback(1);
+        $totalCoresCount = $this->getCountWithFallback(1);
 
-        $availableCpus = $count - $reservedCpus;
+        $availableCpus = max(1, $totalCoresCount - $reservedCpus);
 
-        if (null !== $limit && $availableCpus > $limit) {
-            $availableCpus = $limit;
+        $correctedSystemLoadAverage = null === $systemLoadAverage
+            ? sys_getloadavg()[0] ?? 0.
+            : $systemLoadAverage;
+        $systemLoadAveragePerCore = $correctedSystemLoadAverage / $availableCpus;
+
+        // Adjust available CPUs based on current load
+        if (null !== $loadLimitPerCore && $systemLoadAveragePerCore > $loadLimitPerCore) {
+            $adjustedCpus = max(
+                1,
+                (1 - $systemLoadAveragePerCore) * $availableCpus
+            );
+            $availableCpus = min($availableCpus, $adjustedCpus);
         }
 
-        return max(1, $availableCpus);
+        if (null !== $correctedLimit && $availableCpus > $correctedLimit) {
+            $availableCpus = $correctedLimit;
+        }
+
+        return (int) $availableCpus;
     }
 
     /**
